3716744fbd8fb972347ba04d90f1d9b3b9ffc5e3
letieu <letieu8@gmail.com>
2023-03-03 11:11:10 +0700

    Use write_all instead write

--- src/main.rs
+++ src/main.rs
@@ -26,5 +26,5 @@ fn handle_connection(mut stream: TcpStream) {
 
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
-    stream.write(response.as_bytes()).unwrap();
+    stream.write_all(response.as_bytes()).unwrap();
 }
