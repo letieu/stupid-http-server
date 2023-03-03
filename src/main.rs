374c0141d8aee73524214e1d9b5f38119ccc0156
letieu <letieu8@gmail.com>
2023-03-03 11:09:04 +0700

    Return simple Html

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream}, fs,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        handle_connection(stream.unwrap());
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+
+    let http_request: Vec<String> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = fs::read_to_string("hello.html").unwrap();
+    let length = contents.len();
+
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write(response.as_bytes()).unwrap();
 }
