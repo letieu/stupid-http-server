41c5146cebfa1c7335832afa32b74174f5bc7eef
letieu <letieu8@gmail.com>
2023-03-05 20:42:45 +0700

    Single thread server

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::{
+    fs,
     io::{prelude::*, BufReader},
-    net::{TcpListener, TcpStream}, fs,
+    net::{TcpListener, TcpStream},
+    thread::sleep,
+    time::Duration,
 };
 
 fn main() {
@@ -14,14 +17,18 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
 
-    let http_request: Vec<String> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("hello.html").unwrap();
+    let (status_line, file_name) = match request_line.as_str() {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
+    };
+
+    let contents = fs::read_to_string(file_name).unwrap();
     let length = contents.len();
 
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
