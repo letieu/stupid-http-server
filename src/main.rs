7a7cd36205416773cd80a223cb6d2c3ff79d20a7
letieu <letieu8@gmail.com>
2023-03-14 21:30:18 +0700

    Turning Single-Threaded Server into a Multithreaded Server

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,16 @@ use std::{
     time::Duration,
 };
 
+use stupid_http_server::ThreadPool;
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
-        handle_connection(stream.unwrap());
+        pool.execute(| | {
+            handle_connection(stream.unwrap());
+        })
     }
 }
 
